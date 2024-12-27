74b6d6fc5ad09c74fb034b399f76090957a518b6
migvidal <miguelvidaf@outlook.com>
2024-12-27 17:43:54 +0100

    Get pagination to work, but too many network requests

--- app/src/main/java/com/migvidal/myfirstpaging/ui/MainScreen.kt
+++ app/src/main/java/com/migvidal/myfirstpaging/ui/MainScreen.kt
@@ -1,9 +1,9 @@
 package com.migvidal.myfirstpaging.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -14,6 +14,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemKey
 import com.migvidal.myfirstpaging.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -26,14 +27,22 @@ fun MainScreen(modifier: Modifier = Modifier) {
         }
     ) { innerPadding ->
         val viewModel: MainViewModel = viewModel(factory = MainViewModel.factory)
-        val data = viewModel.data.collectAsLazyPagingItems()
-        if (data.itemCount == 0) return@Scaffold
-        val results = data[0]?.query?.search ?: emptyList()
-        LazyColumn(modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)) {
-            items(results) {
-                Text(text = it.title, modifier = Modifier.padding(16.dp))
+        val pagingItems = viewModel.data.collectAsLazyPagingItems()
+        if (pagingItems.itemCount == 0) return@Scaffold
+        val results = pagingItems[0]?.query?.search ?: emptyList()
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            items(
+                count = pagingItems.itemCount) { index: Int ->
+                val element = pagingItems.get(index) ?: return@items
+                Column {
+                    element.query.search.forEach {
+                        Text(text = it.title, modifier = Modifier.padding(16.dp))
+                    }
+                }
             }
         }
     }
